49a77b1f5624654327c7e5bb5fe45f56201b98ee
sabeeh01 <sabeeh01>
2024-02-26 15:49:34 +0500

    static bundle names generation

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = function override(config, env) {
+  config.output = {
+    ...config.output,
+    filename: "static/js/main.chatbot.js",
+    chunkFilename: "static/js/chatbot.chunk.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+  };
+
+  config.plugins = [
+    ...config.plugins,
+    new MiniCssExtractPlugin({
+      filename: "static/css/main.chatbot.css",
+    }),
+  ];
+
+  return config;
+};
